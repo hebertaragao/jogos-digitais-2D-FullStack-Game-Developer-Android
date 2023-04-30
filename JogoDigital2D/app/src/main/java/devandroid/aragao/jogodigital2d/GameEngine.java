8d72e4dfe5c9b6cea646776a187903481483ea81
heber <hebert.aragao@outlook.com>
2023-04-30 14:30:56 -0300

    Primeiro Commit jogos 2D criado a classe GameEngine

---
+++ JogoDigital2D/app/src/main/java/devandroid/aragao/jogodigital2d/GameEngine.java
@@ -0,0 +1,10 @@
+package devandroid.aragao.jogodigital2d;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameEngine extends SurfaceView {
+    public GameEngine(Context context) {
+        super(context);
+    }
+}
