3e67033fec5c39f2ab38126aaa4f8a2416c6643c
heber <hebert.aragao@outlook.com>
2023-05-05 16:30:23 -0300

    corrigindo warning no catch

--- JogoDigital2D/app/src/main/java/devandroid/aragao/jogodigital2d/MainActivity.java
+++ JogoDigital2D/app/src/main/java/devandroid/aragao/jogodigital2d/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
     ConstraintLayout constraintLayout;
@@ -11,7 +14,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+// Inicio de tela cheia
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        try {
+            getSupportActionBar().hide();
+        } catch (Exception e) {
+            Log.e("LogGame", "onCreate:" + e.getMessage());
+        }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+        //final da tela cheia
 
         constraintLayout = findViewById(R.id.container);
 
