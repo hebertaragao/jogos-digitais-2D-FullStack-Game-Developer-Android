749e1c6f81335d086eeab95a7e75e65e40f9d080
heber <hebert.aragao@outlook.com>
2023-05-06 14:20:13 -0300

    Implementação da classe cores

---
+++ JogoDigital2D/app/src/main/java/devandroid/aragao/jogodigital2d/Cores.java
@@ -0,0 +1,18 @@
+package devandroid.aragao.jogodigital2d;
+
+import android.graphics.Paint;
+
+public class Cores {
+    public static Paint corTextoPontos(){
+        Paint corTextoPontos = new Paint();
+        corTextoPontos.setColor(0xFFFF0000);
+        return corTextoPontos;
+    }
+    public static Paint corGameOver(){
+        Paint corGameOver = new Paint();
+        corGameOver.setColor(0XFFFF0000);
+        return corGameOver;
+    }
+
+
+}
