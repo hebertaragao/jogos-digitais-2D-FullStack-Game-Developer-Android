dc81e19ce1077c1b50da0b6293fd07094377fb05
heber <hebert.aragao@outlook.com>
2023-05-06 14:33:52 -0300

    Implementação da classe cenário

---
+++ JogoDigital2D/app/src/main/java/devandroid/aragao/jogodigital2d/Cenario.java
@@ -0,0 +1,17 @@
+package devandroid.aragao.jogodigital2d;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class Cenario {
+    private DisplayMetrics metrics;
+    public Cenario(Context context){
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = manager.getDefaultDisplay();
+        metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+
+    }
+}
