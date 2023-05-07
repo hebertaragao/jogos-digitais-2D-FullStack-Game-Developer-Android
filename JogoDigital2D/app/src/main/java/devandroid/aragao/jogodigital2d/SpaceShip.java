8954e49ead164d1f4170ea32ae74c3c7aab5db1c
heber <hebert.aragao@outlook.com>
2023-05-06 23:07:55 -0300

    Implementação do construtor da classe SpaceShip

--- JogoDigital2D/app/src/main/java/devandroid/aragao/jogodigital2d/SpaceShip.java
+++ JogoDigital2D/app/src/main/java/devandroid/aragao/jogodigital2d/SpaceShip.java
@@ -2,6 +2,7 @@ package devandroid.aragao.jogodigital2d;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
 public class SpaceShip {
@@ -15,13 +16,23 @@ public class SpaceShip {
 
     private Cenario cenario;
 
+    private Context context;
+
     private Bitmap spaceShip;
 
     boolean isRodape = false;
 
     int bordaSuperior;
 
-    public SpaceShip(Cenario cenario, Context context, Som som){}
+    public SpaceShip(Cenario cenario, Context context, Som som){
+        this.cenario = cenario;
+        this.som = som;
+        this.context = context;
+
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.space_ship);
+        this.spaceShip = Bitmap.createScaledBitmap(bitmap, RAIO*2, RAIO*2, false);
+
+    }
 
     public void adicionarCamada(Canvas canvas){}
 
